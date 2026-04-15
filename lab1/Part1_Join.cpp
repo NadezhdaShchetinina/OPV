f97bf011916f8c11d8541d735d296108e9e0d14a
NadezhdaShchetinina <118117142+NadezhdaShchetinina@users.noreply.github.com>
2026-04-15 18:31:03 +0300

    Add files via upload

---
+++ lab1/Part1_Join.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+#include <thread>
+#include <chrono>
+#include <random>
+using namespace std;
+
+void printNumbers(int threadId) {
+    // Генератор случайных задержек
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<> dis(200, 300);
+
+    for (int i = 1; i <= 5; ++i) {
+        std::cout << "Поток " << threadId << ": " << i << std::endl;
+
+        std::this_thread::sleep_for(std::chrono::milliseconds(dis(gen)));
+
+    }
+
+}
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+    std::cout << "Главный поток: запускаем потоки..." << std::endl;
+
+    std::thread t1(printNumbers, 1);
+    std::thread t2(printNumbers, 2);
+
+    std::cout << "Главный поток: потоки созданы, ожидаем их завершения..." << std::endl;
+
+    for (int i = 1; i <= 3; ++i) {
+        std::cout << "Главный поток: сообщение " << i << std::endl;
+        std::this_thread::sleep_for(std::chrono::milliseconds(150));
+
+    }
+
+    t1.join();
+    t2.join();
+
+    std::cout << "Главный поток: все потоки завершены. Программа завершается." << std::endl;
+
+    return 0;
+
+}
---
+++ lab1/Part2_Detach.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+#include <thread>
+#include <chrono>
+#include <random>
+using namespace std;
+
+void printNumbers(int threadId, int delayMs) {
+
+    for (int i = 1; i <= 5; ++i) {
+        std::cout << "Поток " << threadId << ": " << i << std::endl;
+
+        std::this_thread::sleep_for(std::chrono::milliseconds(delayMs));
+
+    }
+    std::cout << "Поток " << threadId << " завершил работу" << std::endl;
+}
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+    std::cout << "Главный поток: запускаем потоки..." << std::endl;
+
+    // Создаём потоки с разными задержками
+    std::thread t1(printNumbers, 1, 200);
+    std::thread t2(printNumbers, 2, 800);
+
+    std::cout << "Главный поток: потоки созданы" << std::endl;
+
+    t1.detach();
+    std::cout << "Главный поток: поток 1 отсоединён (detach)  " << std::endl;
+
+    for (int i = 1; i <= 5; ++i) {
+        std::cout << "Работа главного потока" << i << std::endl;
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+    }
+    std::cout << "Главный поток: ожидаем завершения потока 2 (join)  " << std::endl;
+    t2.join();
+
+    std::cout << "Главный поток: дополнительная задержка  " << std::endl;
+    std::this_thread::sleep_for(std::chrono::seconds(3));
+
+    std::cout << "Главный поток: программа завершается" << std::endl;
+
+    return 0;
+}
